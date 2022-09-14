c1823b3ab7b811e503cddd36b9aad2c692e55108
Ace <alvincennanda@gmail.com>
2022-09-14 13:07:47 +0700

    Initial commit

---
+++ app/src/main/java/com/example/view_binding_10795/TaskList.kt
@@ -0,0 +1,12 @@
+package com.example.view_binding_10795
+
+object TaskList {
+
+    val taskList = listOf<Task>(
+        Task("Chapter 1", "Bangun Tidur","Kebiasaan sehari - hari untuk membuat hari makin Indah"),
+        Task("Chapter 2", "Main Mobile Legend", "Mencari Point Rank"),
+        Task("Chapter 3", "Belajar Web", "Nonton Youtube Web Unpas"),
+        Task("Chapter 4", "Makan", "Makan di motekar"),
+        Task("Chapter 5", "Tiktok an","Menonton Tiktok Chrislin")
+    )
+}
